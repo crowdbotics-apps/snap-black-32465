ff9f38e00d3b36e30e66dac4e0a77302d536d815
Crowdbotics <team@crowdbotics.com>
2021-12-17 11:55:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "snap_black_32465",
+  url: "https://snap_black_32465.botics.co",
+  api: "https://snap_black_32465.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
